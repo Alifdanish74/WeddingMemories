6db3b82486665b00bd46933625fe88011d5e2fb8
Alif Danish <46293172+Alifdanish74@users.noreply.github.com>
2024-07-25 00:20:52 +0800

    k

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -17,7 +17,7 @@ function App() {
     formData.append('file', state.file);
 
     try {
-      const response = await axios.post('https://wedding-memories-server-1xhfzhmfz-alif-danishs-projects.vercel.app/upload', formData, {
+      const response = await axios.post('http://localhost:5000/upload', formData, {
         headers: {
           'Content-Type': 'multipart/form-data',
         },
